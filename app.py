cb76c5dde71b0591662ae0375668169ec3fad9a9
Rafael Brito <rafaelbritohs@gmail.com>
2023-07-11 18:10:11 -0300

    Blocks funcionando

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 App = Flask(__name__)
 
 @App.route('/')
 def index():
-    return 'Olá mundo'
+    return render_template('index.html')
+
+@App.route('/produto')
+def produto():
+    return render_template('produto.html')
