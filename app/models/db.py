54d9de10d1f4770738d223d95021a7ba40649072
Rafael Brito <rafael.britohs@gmail.com>
2023-07-21 23:00:31 -0300

    criada funcao sql do banco

--- app/models/db.py
+++ app/models/db.py
@@ -1,3 +1,17 @@
 import sqlite3
 
 db = sqlite3.connect('loja.db',check_same_thread=False)
+
+def sql(querry,operacao):
+    if(operacao=='select'):
+        cursor = db.cursor()
+        cursor.execute(querry)
+        dados = cursor.fetchall()
+        cursor.close()
+        return dados
+    if(operacao=='create' or operacao=='insert' or operacao=='update' or operacao=='delete'):
+        cursor = db.cursor()
+        cursor.execute(querry)
+        db.commit()
+        cursor.close()
+        return "Operação realizada com sucesso"
--- app/models/produtos.py
+++ app/models/produtos.py
@@ -1,53 +1,33 @@
-from app.models.db import db
+from app.models import db
 from app.models.categorias import categorias
 
 class produtos():
 
     def criar():
-        cursor = db.cursor()
         sql = "CREATE TABLE IF NOT EXISTS 'produtos' (id INTEGER PRIMARY KEY AUTOINCREMENT,nome varchar[30] NOT NULL,preco float[7,2] NOT NULL,foto varchar[100] NOT NULL,cat_fk INTEGER NOT NULL,FOREIGN KEY (cat_fk) REFERENCES categorias(cat_id));"
-        cursor.execute(sql)
-        cursor.close()
+        db.sql(sql,'create')
+        
     def inserir(nome,preco,foto,cat):
-        cursor = db.cursor()
         sql=f"INSERT INTO produtos (rowid,nome,preco,foto,cat_fk) VALUES (NULL,'{nome}','{preco}','{foto}','{cat}');"
-        cursor.execute(sql)
-        db.commit()
-        cursor.close()
+        db.sql(sql,'insert')
+        
     def selecionar_tudo():
-        cursor = db.cursor()
         sql= "SELECT * FROM produtos;"
-        cursor.execute(sql)
-        dados = cursor.fetchall()
-        cursor.close()
-        return dados
+        return db.sql(sql,'select')
         
     def selecionar_categoria(cat):
-        cursor = db.cursor()
         cat_id = categorias.selecionar(cat)
         sql= f"SELECT * FROM produtos WHERE cat_fk={cat_id[0][0]};"
-        cursor.execute(sql)
-        dados = cursor.fetchall()
-        cursor.close()
-        return dados
+        return db.sql(sql,'select')
         
     def selecionar_produto(id):
-        cursor = db.cursor()
         sql= f"SELECT * FROM produtos WHERE id={id};"
-        cursor.execute(sql)
-        dados = cursor.fetchall()
-        cursor.close()
-        return dados
+        return db.sql(sql,'select')
+        
     def atualizar(id,nome,preco,foto,cat_fk):
-        cursor = db.cursor()
         sql= f"UPDATE produtos SET nome='{nome}',preco='{preco}',foto='{foto}',cat_fk='{cat_fk}' WHERE id={id};"
-        cursor.execute(sql)
-        db.commit()
-        cursor.close()
+        db.sql(sql,'update')
+        
     def deletar(id):
-        cursor = db.cursor()
         sql= f"DELETE FROM produtos where id={id};"
-        cursor.execute(sql)
-        dados = cursor.fetchall()
-        cursor.close()
-        return dados
+        return db.sql(sql,'delete')
